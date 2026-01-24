8e769478d212c1f096d742245b7413a83bb137c8
uhyo <uhyo@uhy.ooo>
2026-01-24 10:06:03 +0900

    docs: add scroll to top workaround

--- packages/docs/src/App.tsx
+++ packages/docs/src/App.tsx
@@ -8,6 +8,7 @@ import { ApiHooksPage } from "./pages/ApiHooksPage.js";
 import { ApiUtilitiesPage } from "./pages/ApiUtilitiesPage.js";
 import { ApiTypesPage } from "./pages/ApiTypesPage.js";
 import { ExamplesPage } from "./pages/ExamplesPage.js";
+import { useEffect } from "react";
 
 const routes = [
   route({
@@ -53,5 +54,32 @@ const routes = [
 ];
 
 export function App() {
+  // Auto scroll to top - this should be handled by the browser per spec,
+  // but currently Chrome and Safari do not follow the spec.
+  useEffect(() => {
+    // @ts-expect-error -- TypeScript does not yet know about the Navigation API
+    const navigation = window.navigation;
+    if (!navigation) {
+      return;
+    }
+    const controller = new AbortController();
+    navigation.addEventListener(
+      "navigatesuccess",
+      () => {
+        const transition = navigation.transition;
+        if (
+          transition.navigationType === "push" ||
+          transition.navigationType === "replace"
+        ) {
+          window.scrollTo(0, 0);
+        }
+      },
+      { signal: controller.signal },
+    );
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
   return <Router routes={routes} fallback="static" />;
 }
